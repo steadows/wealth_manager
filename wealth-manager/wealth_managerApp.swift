49a07e02876cd4850d6ba912c6f4e7858289ca63
Steve Meadows <56893550+steadows@users.noreply.github.com>
2026-03-07 16:48:04 -0500

    Initial Commit

---
+++ wealth-manager/wealth_managerApp.swift
@@ -0,0 +1,32 @@
+//
+//  wealth_managerApp.swift
+//  wealth-manager
+//
+//  Created by Steve Meadows on 3/7/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct wealth_managerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ wealth-managerTests/wealth_managerTests.swift
@@ -0,0 +1,16 @@
+//
+//  wealth_managerTests.swift
+//  wealth-managerTests
+//
+//  Created by Steve Meadows on 3/7/26.
+//
+
+import Testing
+
+struct wealth_managerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
